8cebd15f3464d65b169786d39a9dfba790a5c396
Kent Fredric <kentfredric@gmail.com>
2015-09-28 08:46:13 +1300

    Use a ternary to return the namespace value

--- lib/PPIx/Element/Package.pm
+++ lib/PPIx/Element/Package.pm
@@ -62,11 +62,7 @@ sub identify_package_namespace {
   my ($element) = @_;
   my $package = identify_package($element);
 
-  return 'main' unless defined $package;
-
-  return 'main' unless defined $package->namespace;
-
-  return $package->namespace;
+  return ( defined $package and defined $package->namespace ) ? $package->namespace : 'main';
 }
 
 =func identify_package_in_previous_siblings
